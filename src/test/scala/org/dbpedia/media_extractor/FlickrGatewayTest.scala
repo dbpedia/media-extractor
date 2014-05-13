bbd468076994731b75297936044942f50bfa6b79
Leandro Doctors <ldoctors@gmail.com>
2014-05-13 11:22:31 +0200

    Add FlickrGatewayTest (without Flickr API key)

---
+++ src/test/scala/org/dbpedia/media_extractor/FlickrGatewayTest.scala
@@ -0,0 +1,36 @@
+/**
+ *
+ */
+package org.dbpedia.media_extractor
+
+import org.scalatest.FunSpec
+import com.flickr4java.flickr.test.TestInterface
+import com.flickr4java.flickr.Flickr
+import com.flickr4java.flickr.REST
+import java.util.HashMap
+
+/**
+ * @author allentiak
+ *
+ */
+object FlickrGatewayTest extends FunSpec{
+  
+  describe("A FlickrGateway") {
+    it("should be able to exist :-)") (pending)
+//    {
+//      val myAPIKey="myKey"
+//      val mySecret="mySecret"
+//      val myTransport:REST=new REST()
+//      var myFlickrGateway= new Flickr(myAPIKey,mySecret,myTransport)
+//      val mytestInterface = myFlickrGateway.getTestInterface()
+//      var myHashMap= new HashMap[String,String]
+//      myHashMap.put("Marco","Polo")
+//      val results = mytestInterface.echo(myHashMap)
+//
+//    }
+//    (pending)
+  }
+
+ // def main(args: Array[String]): Unit = {}
+
+}
