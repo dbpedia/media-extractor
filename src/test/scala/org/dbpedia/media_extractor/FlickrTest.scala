06214bfc694608f8bff7cd4896f031e31fde51c2
Leandro Doctors <ldoctors@gmail.com>
2014-05-15 21:47:59 +0200

    FlickrTest is now a Scala version of Flickr4Java's AuthExample (yet to be converted into a proper test)

--- src/test/scala/org/dbpedia/media_extractor/FlickrTest.scala
+++ src/test/scala/org/dbpedia/media_extractor/FlickrTest.scala
@@ -10,69 +10,87 @@ import org.scalatest.FunSpec
 import com.flickr4java.flickr.Flickr
 import com.flickr4java.flickr.FlickrException
 import com.flickr4java.flickr.REST
-import com.flickr4java.flickr.auth.Auth;
-import com.flickr4java.flickr.auth.AuthInterface;
-import com.flickr4java.flickr.auth.Permission;
-import com.flickr4java.flickr.util.IOUtilities;
+import com.flickr4java.flickr.auth.Auth
+import com.flickr4java.flickr.auth.AuthInterface
+import com.flickr4java.flickr.auth.Permission
+import com.flickr4java.flickr.util.IOUtilities
 
-import org.scribe.model.Token;
-import org.scribe.model.Verifier;
+import org.scribe.model.Token
+import org.scribe.model.Verifier
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-import java.util.Scanner;
+import java.io.IOException
+import java.io.InputStream
+import java.util.Properties
+import java.util.Scanner
 
 /**
  * @author allentiak
  *
  */
-object FlickrTest extends FunSpec {
+class FlickrTest extends FunSpec {
 
   describe("A Flickr instance") {
-       
-    var properties: Properties = null
 
-    def auth() {      
-      var in: InputStream = null;
-      try {
-        var in = classOf[AuthExample].getResourceAsStream("/flickr.setup.properties");
-        properties = new Properties();
-        properties.load(in);
-      } finally {
-        IOUtilities.close(in);
+    it("should be able to connect to Flickr") {
+      var properties: Properties = null
+
+      def auth() = {
+        var in: InputStream = null
+        try {
+          var in = classOf[FlickrTest].getResourceAsStream("/flickr.setup.properties")
+          properties = new Properties()
+          properties.load(in)
+        } finally {
+          IOUtilities.close(in)
+        }
+
+        val myFlickr = new Flickr(properties.getProperty("apiKey"), properties.getProperty("secret"), new REST())
+
+        Flickr.debugStream = false
+        val authInterface = myFlickr.getAuthInterface()
+
+        val scanner = new Scanner(System.in)
+
+        val token = authInterface.getRequestToken()
+        println("token: " + token)
+
+        val url = authInterface.getAuthorizationUrl(token, Permission.READ)
+        println("Follow this URL to authorise yourself on Flickr")
+        println(url)
+        println("Paste in the token it gives you:")
+        print(">>")
+
+        val tokenKey: String = scanner.nextLine()
+        scanner.close()
+
+        val requestToken = authInterface.getAccessToken(token, new Verifier(tokenKey))
+        println("Authentication success")
+
+        val auth = authInterface.checkToken(requestToken)
+
+        // This token can be used until the user revokes it.
+        println("Token: " + requestToken.getToken())
+        println("nsid: " + auth.getUser().getId())
+        println("Realname: " + auth.getUser().getRealName())
+        println("Username: " + auth.getUser().getUsername())
+        println("Permission: " + auth.getPermission().getType())
+
+        auth
       }
-    }
 
-    val myFlickr = new Flickr(properties.getProperty("apiKey"), properties.getProperty("secret"), new REST());
-
-    var myHashMap = new HashMap[String, String]
-    myHashMap.put("api_key", myAPIKey)
-
-    it("should throw a FlickrException if no API key is supplied when echoing") {
-
-      val myTestInterface = myFlickr.getTestInterface()
-      intercept[FlickrException] {
-        var testInterfaceResults = myTestInterface.echo(new HashMap)
+      def main(args: Array[String]=null):Int ={
+        try {
+          auth()
+        } catch {
+          case e: Exception => e.printStackTrace()
+        }
+        0
       }
-    }
-
-    it("should echo \"api_key\" if the API key is supplied when echoing") {
-
-      val myTestInterface = myFlickr.getTestInterface()
-      var testInterfaceResults = myTestInterface.echo(myHashMap)
-      assert(testInterfaceResults.toString().contains("api_key"))
-    }
-
-    it("should echo \"method\" if the API key is supplied when echoing") {
-
-      val myTestInterface = myFlickr.getTestInterface()
-      var testInterfaceResults = myTestInterface.echo(myHashMap)
-      assert(testInterfaceResults.toString().contains("method"))
+      
+      assert (main()===0)
     }
 
     it("should get at least one picture")(pending)
 
   }
-
 }
