9cdcc0cfad8898fb0a7c9c33de3559e6df4bf3b7
Leandro Doctors <ldoctors@gmail.com>
2014-05-21 22:03:57 +0200

    Rename class FlickrTest to FlickrRestApiTest

---
+++ src/test/scala/org/dbpedia/media_extractor/FlickrRestApiTest.scala
@@ -0,0 +1,20 @@
+/**
+ *
+ */
+package org.dbpedia.media_extractor
+
+
+
+/**
+ * @author allentiak
+ *
+ */
+class FlickrRestApiTest extends FunSpec {
+
+  describe("A Flickr instance") {
+
+    it("should be able to connect to Flickr") (pending)
+    it("should get at least one picture")(pending)
+
+  }
+}
