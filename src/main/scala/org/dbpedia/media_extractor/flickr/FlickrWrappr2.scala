4370578d938877cf95314c5fda78a50203432b53
Leandro Doctors <ldoctors@gmail.com>
2014-07-02 16:00:37 +0200

    Separate OAuth functionality

--- src/main/scala/org/dbpedia/media_extractor/flickr/FlickrWrappr2.scala
+++ src/main/scala/org/dbpedia/media_extractor/flickr/FlickrWrappr2.scala
@@ -1,17 +1,13 @@
 package org.dbpedia.media_extractor.flickr
 
-import scala.xml.Elem
-import scala.collection.mutable.ListBuffer
-import java.util.Properties
 import java.net.URI
-import org.scribe.builder.api.FlickrApi
-import org.scribe.builder.ServiceBuilder
-import java.util.Scanner
-import org.scribe.model.Verifier
-import org.scribe.model.Token
+
+import scala.collection.mutable.ListBuffer
+import scala.xml.Elem
+
 import org.scribe.model.OAuthRequest
-import org.scribe.model.Verb
 import org.scribe.model.Response
+import org.scribe.model.Verb
 
 case class SearchResult(depictionUri: String, pageUri: String)
 
@@ -29,51 +25,7 @@ object FlickrWrappr2 extends App {
     }
     resultsListBuffer.toList
   }
-
-  def flickrAuth(credentialsFile: String) = {
-
-    val accessCredentials = new Properties()
-
-    val inputFile = this.getClass().getResourceAsStream(credentialsFile)
-    accessCredentials.load(inputFile)
-    inputFile.close()
-
-    val myFlickrService = new ServiceBuilder()
-      .provider(classOf[FlickrApi])
-      .apiKey(accessCredentials.getProperty("apiKey"))
-      .apiSecret(accessCredentials.getProperty("apiKeySecret"))
-      .build()
-
-    val requestToken = myFlickrService.getRequestToken()
-    val authorizationUri = myFlickrService.getAuthorizationUrl(requestToken)
-
-    println("Follow this authorization URL to authorise yourself on Flickr:")
-    println(authorizationUri)
-    println("Paste here the verifier it gives you:")
-    print(">>")
-
-    val scanner = new Scanner(System.in)
-    val verifier = new Verifier(scanner.nextLine())
-    scanner.close()
-
-    println("")
-
-    val accessToken = myFlickrService.getAccessToken(requestToken, verifier)
-    println("Authentication success")
-  }
-
-  def getSavedFlickrAccessToken(accessTokenFile: String): Token = {
-    val accessCredentials = new Properties()
-    val inputFile = this.getClass().getResourceAsStream(accessTokenFile)
-    accessCredentials.load(inputFile)
-    inputFile.close()
-
-    val accessToken = accessCredentials.getProperty("accessToken")
-    val accessSecret = accessCredentials.getProperty("accessSecret")
-
-    new Token(accessToken, accessSecret)
-  }
-
+  
   def getFlickrSearchResponse(text: String = "", latitude: String = "", longitude: String = "", license: String = ""): Response = {
     val searchRequest = new OAuthRequest(Verb.POST, endPointUri.toString())
 
---
+++ src/main/scala/org/dbpedia/media_extractor/flickr/OAuthSession.scala
@@ -0,0 +1,61 @@
+/**
+ *
+ */
+package org.dbpedia.media_extractor.flickr
+
+import org.scribe.model.Verifier
+import org.scribe.builder.api.FlickrApi
+import org.scribe.builder.ServiceBuilder
+import java.util.Properties
+import java.util.Scanner
+import org.scribe.model.Token
+
+/**
+ * @author allentiak
+ *
+ */
+object OAuthSession {
+  def flickrAuth(credentialsFile: String) = {
+
+    val accessCredentials = new Properties()
+
+    val inputFile = this.getClass().getResourceAsStream(credentialsFile)
+    accessCredentials.load(inputFile)
+    inputFile.close()
+
+    val myFlickrService = new ServiceBuilder()
+      .provider(classOf[FlickrApi])
+      .apiKey(accessCredentials.getProperty("apiKey"))
+      .apiSecret(accessCredentials.getProperty("apiKeySecret"))
+      .build()
+
+    val requestToken = myFlickrService.getRequestToken()
+    val authorizationUri = myFlickrService.getAuthorizationUrl(requestToken)
+
+    println("Follow this authorization URL to authorise yourself on Flickr:")
+    println(authorizationUri)
+    println("Paste here the verifier it gives you:")
+    print(">>")
+
+    val scanner = new Scanner(System.in)
+    val verifier = new Verifier(scanner.nextLine())
+    scanner.close()
+
+    println("")
+
+    val accessToken = myFlickrService.getAccessToken(requestToken, verifier)
+    println("Authentication success")
+  }
+
+  def getSavedFlickrAccessToken(accessTokenFile: String): Token = {
+    val accessCredentials = new Properties()
+    val inputFile = this.getClass().getResourceAsStream(accessTokenFile)
+    accessCredentials.load(inputFile)
+    inputFile.close()
+
+    val accessToken = accessCredentials.getProperty("accessToken")
+    val accessSecret = accessCredentials.getProperty("accessSecret")
+
+    new Token(accessToken, accessSecret)
+  }
+}
