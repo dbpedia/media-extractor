bc8f1b5034a1771205c1abd02ca3226b3f70c3ef
Leandro Doctors <ldoctors@gmail.com>
2014-07-12 20:59:42 +0200

    Bugfix: Replace: "it" -> "describe"

--- src/test/scala/org/dbpedia/media_extractor/flickr/FlickrRestTest.scala
+++ src/test/scala/org/dbpedia/media_extractor/flickr/FlickrRestTest.scala
@@ -13,7 +13,7 @@ class FlickrRestApiTest extends FunSpec {
 
   describe("A Flickr instance") {
 
-    it("should load non-empty Flickr credentials from an external file, generate a session with those credentials and use that session to invoke Flickr methods") {
+    describe("should load non-empty Flickr credentials from an external file, generate a session with those credentials and use that session to invoke Flickr methods") {
       // I don't know how to correctly test whether the instance was successfully created
       // Maybe adding a return type of 0 (zero) to the constructor?
       val flickrOAuthSession = FlickrOAuthSession("/flickr.setup.properties")
@@ -24,44 +24,42 @@ class FlickrRestApiTest extends FunSpec {
         assert(generatedToken === savedToken)
       }
 
-      it("should invoke test methods") {
+      describe("should invoke test methods (unsigned)") {
         it("should invoke method 'flickr.test.echo'") {
           val echoResponse = flickrOAuthSession.invoke_parameterless_method("flickr.test.echo")
           assert(echoResponse.getMessage() === "OK")
         }
+        it("should invoke method 'flickr.test.login'") {
+          val nullResponse = flickrOAuthSession.invoke_parameterless_method("flickr.test.null", false)
+          assert(nullResponse.getMessage() === "OK")
+        }
+      }
 
         it("should invoke method 'flickr.test.login'") {
           val loginResponse = flickrOAuthSession.invoke_parameterless_method("flickr.test.login")
           assert(loginResponse.getMessage() === "OK")
         }
-
         it("should invoke method 'flickr.test.null'") {
           val nullResponse = flickrOAuthSession.invoke_parameterless_method("flickr.test.null")
           assert(nullResponse.getMessage() === "OK")
         }
       }
 
-      it("should invoke method 'flickr.photos.search'") {
-
+      describe("should invoke method 'flickr.photos.search'") {
         it("should get at least one picture") {
-
           /*
         Brussels:
         latitude: 50°51′0″N
         longitude: 4°21′0″E
         */
-
           val searchText = ""
           val lat = "50.85"
           val lon = "4.35"
-
           val license = "1,2"
 
           val searchResponse = FlickrOAuthSession.getFlickrSearchResponse(searchText, lat, lon, license)
           assert(searchResponse.getMessage() === "OK")
-
         }
-
         it("should show the photos' links")(pending)
 
       }
