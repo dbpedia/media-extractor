8626f1a7b83ea0076bc8239e80325e690bf20648
Leandro Doctors <ldoctors@gmail.com>
2014-07-24 18:05:20 +0200

    Delete unused autogenerated namespaces

--- src/main/scala/org/dbpedia/media_extractor/flickr/FlickrSearch.scala
+++ src/main/scala/org/dbpedia/media_extractor/flickr/FlickrSearch.scala
@@ -9,14 +9,6 @@ import com.hp.hpl.jena.vocabulary.RDFS
 case class FlickrSearchResult(depictionUri: String, pageUri: String)
 
 abstract class FlickrSearch {
-  // Namespaces
-
-  // Auto-generated Namespaces
-  protected val rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-  protected val xsd = "http://www.w3.org/2001/XMLSchema#"
-  protected val owl = "http://www.w3.org/2002/07/owl#"
-  protected val dc = "http://purl.org/dc/elements/1.1/"
-  protected val vcard = "http://www.w3.org/2001/vcard-rdf/3.0#"
 
   protected val namespacesMap = Map(
     "foaf" -> "http://xmlns.com/foaf/0.1/",
