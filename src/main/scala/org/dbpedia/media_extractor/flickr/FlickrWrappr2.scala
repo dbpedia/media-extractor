bef86e4165623d3c6edba57f53856e454236390f
Leandro Doctors <ldoctors@gmail.com>
2014-07-27 12:23:54 +0200

    Add FlickrWrappr2 members and default parameters

--- src/main/scala/org/dbpedia/media_extractor/flickr/FlickrWrappr2.scala
+++ src/main/scala/org/dbpedia/media_extractor/flickr/FlickrWrappr2.scala
@@ -4,13 +4,34 @@ class FlickrWrappr2(val serverRootUri: String = "http://localhost/flickrwrappr/"
   val locationRootUri = serverRootUri + "location/"
   val dataRootUri = serverRootUri + "data/photosDepictingLocation/"
   val flickrOAuthSession = FlickrOAuthSession(flickrCredentialsFile)
+
+  // TODO: allow user to input search parameters.
+  // (For now, search for Brussels by default)
+
+  // Geo default parameters:
+  val lat = "50.85"
+  val lon = "4.35"
+  val radius = "5"
+
+  // DBpedia default parameter:
+  val targetResource = "Brussels"
+
+  val flickrGeoLookup: FlickrLookup = FlickrGeoLookup(
+    lat = lat,
+    lon = lon,
+    radius = radius,
+    locationRootUri = locationRootUri,
+    dataRootUri = dataRootUri,
+    serverRootUri = serverRootUri,
+    flickrOAuthSession = flickrOAuthSession)
+
+  val flickrDBpediaLookup: FlickrLookup = FlickrDBpediaLookup(
+    targetResource = targetResource,
+    serverRootUri = serverRootUri,
+    flickrOAuthSession = flickrOAuthSession)
 }
 
 object FlickrWrappr2 extends App {
-  val flickrOAuthSession = FlickrWrappr2.flickrOAuthSession(flickrCredentialsFile)
-  var flickrGeoSearch: FlickrGeoSearch = null
-  var flickrDBpediaSearch: FlickrDBpediaSearch = null
-
   def apply(serverRootUri: String = "http://localhost/flickrwrappr/", flickrCredentialsFile: String = "/flickr.setup.properties") =
     new FlickrWrappr2(serverRootUri, flickrCredentialsFile)
 
