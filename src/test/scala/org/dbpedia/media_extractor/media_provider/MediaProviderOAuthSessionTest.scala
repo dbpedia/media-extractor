c0306cecce3975468f7eb518077b612d200af0e2
Leandro Doctors <ldoctors@gmail.com>
2014-08-11 18:50:08 +0200

    Add MediaProviderOAuthSessionTest stub

---
+++ src/test/scala/org/dbpedia/media_extractor/media_provider/MediaProviderOAuthSessionTest.scala
@@ -0,0 +1,19 @@
+package org.dbpedia.media_extractor.media_provider
+
+import org.scalatest.FunSpec
+
+class MediaProviderOAuthSessionTest extends FunSpec {
+
+  describe("A MediaProviderOAuthSession instance") {
+
+    it("should be able to connect to Flickr using saved credentials") {
+      // TODO: complete stub
+
+    }
+
+    it("should be able to connect to YouTube using saved credentials") {
+      // TODO: complete stub
+    }
+
+  }
+}
