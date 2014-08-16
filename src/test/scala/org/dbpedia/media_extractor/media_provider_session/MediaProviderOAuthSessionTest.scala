a74f809040d782685f233faa0b3cf9a2259a5d28
Leandro Doctors <ldoctors@gmail.com>
2014-08-16 22:43:29 +0200

    Add YouTubeMediaProviderOAuthSessionTest (needs Google OAuth 2.0 support from scribe)

--- src/test/scala/org/dbpedia/media_extractor/media_provider_session/MediaProviderOAuthSessionTest.scala
+++ src/test/scala/org/dbpedia/media_extractor/media_provider_session/MediaProviderOAuthSessionTest.scala
@@ -8,7 +8,7 @@ class MediaProviderOAuthSessionTest extends FunSpec {
 
     describe("Flickr") {
 
-      it("should be able to connect to Flickr using saved credentials (only <ApiKey,ApiKeySecret>)") {
+      ignore("should be able to connect to Flickr using saved credentials (only <ApiKey,ApiKeySecret>)") {
         val manuallyGeneratedFlickrOAuthSession = FlickrMediaProviderOAuthSession(
           savedCredentialsFile = "/flickr.setup.properties",
           savedAccessTokenFile = "")
@@ -67,21 +67,65 @@ class MediaProviderOAuthSessionTest extends FunSpec {
     }
 
     describe("YouTube") {
-      // TODO: complete stub
 
       it("should be able to connect to YouTube using saved credentials (only <ApiKey,ApiKeySecret>)") {
-        // TODO: complete stub
+        val manuallyGeneratedYouTubeOAuthSession = YouTubeMediaProviderOAuthSession(
+          savedCredentialsFile = "/youtube.setup.properties",
+          savedAccessTokenFile = "")
+        assert(manuallyGeneratedYouTubeOAuthSession.isInstanceOf[YouTubeMediaProviderOAuthSession])
       }
 
-      it("should be able to connect to YouTube using saved credentials (both <ApiKey,ApiKeySecret> and <accessToken,accessTokenSecret>)") {
-        // TODO: complete stub
+      ignore("should be able to connect to YouTube using saved credentials (both <ApiKey,ApiKeySecret> and <accessToken,accessTokenSecret>)") {
+        val automaticallyGeneratedYouTubeOAuthSession = YouTubeMediaProviderOAuthSession(
+          savedCredentialsFile = "/youtube.setup.properties",
+          savedAccessTokenFile = "/youtube.accessToken.properties")
+        assert(automaticallyGeneratedYouTubeOAuthSession.isInstanceOf[YouTubeMediaProviderOAuthSession])
+
+        val generatedToken = automaticallyGeneratedYouTubeOAuthSession.accessToken
+        val savedToken = automaticallyGeneratedYouTubeOAuthSession.getSavedAccessToken("/youtube.accessToken.properties")
+        assert(generatedToken === savedToken)
       }
 
-      it("should be able to invoke YouTube test methods (TBD)") {
-        // TODO: complete stub
+      ignore("should be able to invoke YouTube test methods (TBD)") {
+
+        val flickrMediaProviderOAuthSession = FlickrMediaProviderOAuthSession(
+          savedCredentialsFile = "/flickr.setup.properties",
+          savedAccessTokenFile = "/flickr.accessToken.properties")
+
+        describe("(unsigned)") {
+          it("method 'flickr.test.echo'") {
+            val unsignedEchoResponse = flickrMediaProviderOAuthSession.invoke_parameterless_method("flickr.test.echo", false)
+            assert(unsignedEchoResponse.getMessage() === "OK")
+          }
+          it("method 'flickr.test.login'") {
+            val unsignedLoginResponse = flickrMediaProviderOAuthSession.invoke_parameterless_method("flickr.test.login", false)
+            assert(unsignedLoginResponse.getMessage() === "OK")
+          }
+          it("method 'flickr.test.null'") {
+            val unsignedNullResponse = flickrMediaProviderOAuthSession.invoke_parameterless_method("flickr.test.null", false)
+            assert(unsignedNullResponse.getMessage() === "OK")
+          }
+        }
+
+        describe("(signed)") {
+          it("method 'flickr.test.echo'") {
+            val signedEchoResponse = flickrMediaProviderOAuthSession.invoke_parameterless_method("flickr.test.echo", true)
+            assert(signedEchoResponse.getMessage() === "OK")
+          }
+          it("method 'flickr.test.login'") {
+            val signedLoginResponse = flickrMediaProviderOAuthSession.invoke_parameterless_method("flickr.test.login", true)
+            assert(signedLoginResponse.getMessage() === "OK")
+          }
+          it("method 'flickr.test.null'") {
+            val signedNullResponse = flickrMediaProviderOAuthSession.invoke_parameterless_method("flickr.test.null", true)
+            assert(signedNullResponse.getMessage() === "OK")
+          }
+        }
+
       }
 
     }
 
   }
+  
 }
