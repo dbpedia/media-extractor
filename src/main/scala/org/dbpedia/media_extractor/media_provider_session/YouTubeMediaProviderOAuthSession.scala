e6c219a4498477f0477f5442a0a00ca1a9818904
Leandro Doctors <ldoctors@gmail.com>
2014-08-16 12:24:10 +0200

    Add full "YouTubeMediaProviderOAuthSession" class stub

--- src/main/scala/org/dbpedia/media_extractor/media_provider_session/YouTubeMediaProviderOAuthSession.scala
+++ src/main/scala/org/dbpedia/media_extractor/media_provider_session/YouTubeMediaProviderOAuthSession.scala
@@ -3,9 +3,24 @@ package org.dbpedia.media_extractor.media_provider_session
 import org.scribe.builder.api.GoogleApi
 
 class YouTubeMediaProviderOAuthSession(
-  targetLicenses: String,
-  mediaProviderOAuthSession: MediaProviderOAuthSession[GoogleApi])
+  savedCredentialsFile: String = "/youtube.setup.properties",
+  savedAccessTokenFile: String = "/youtube.accessToken.properties")
 
-  extends MediaProviderOAuthSession(mediaProviderOAuthSession, targetLicenses) {
+  extends MediaProviderOAuthSession[GoogleApi](
+    myApi = new GoogleApi,
+    targetLicenses = "creativeCommon", // CC-BY (see details below)
+    savedCredentialsFile,
+    savedAccessTokenFile) {
+
+  /*
+   * Accepted licenses in YouTube are:
+   * (according to https://developers.google.com/youtube/v3/docs/videos#properties)
+   * 
+   * 1) YouTube license
+   * 2) Creative Commons Attribution License
+   * (see https://support.google.com/youtube/answer/2797468?hl=en)
+   * 
+   */
+
   // TODO: complete this empty stub
 }
