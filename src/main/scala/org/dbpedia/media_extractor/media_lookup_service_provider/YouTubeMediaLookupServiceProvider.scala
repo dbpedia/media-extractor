411e5fd8e67930a0c535fc275871dc1077890db5
Leandro Doctors <ldoctors@gmail.com>
2014-08-17 23:04:21 +0200

    Implement YouTubeMediaLookupServiceProvider stub

--- src/main/scala/org/dbpedia/media_extractor/media_lookup_service_provider/YouTubeMediaLookupServiceProvider.scala
+++ src/main/scala/org/dbpedia/media_extractor/media_lookup_service_provider/YouTubeMediaLookupServiceProvider.scala
@@ -3,10 +3,36 @@ package org.dbpedia.media_extractor.media_lookup_service_provider
 import org.scribe.model.Verb
 import org.scribe.model.OAuthRequest
 import org.scribe.model.Response
+import org.dbpedia.media_extractor.oauthsession.OAuthSession
+import org.scribe.builder.api.GoogleApi
 
-class YouTubeMediaLookupServiceProvider extends MediaLookupServiceProvider {
+class YouTubeMediaLookupServiceProvider(
 
-  //TODO: complete stub
+  savedCredentialsFile: String = "/youtube.setup.properties",
+  savedAccessTokenFile: String = "/youtube.accessToken.properties")
+
+  extends MediaLookupServiceProvider[GoogleApi]() {
+
+  override val termsOfUseUri = "https://developers.google.com/youtube/terms"
+  override val endPointRootUri = "https://www.googleapis.com/youtube/v3"
+
+  override val maxResultsPerQuery = "50" // according to YouTube API specs
+  override val targetLicenses = "creativeCommon" // CC-BY (see details below)
+
+  override val oAuthSession = new OAuthSession[GoogleApi](
+    new GoogleApi,
+    savedCredentialsFile,
+    savedAccessTokenFile)
+
+  /*
+   * Accepted licenses in YouTube are:
+   * (according to https://developers.google.com/youtube/v3/docs/videos#properties)
+   * 
+   * 1) YouTube license
+   * 2) Creative Commons Attribution License
+   * (see https://support.google.com/youtube/answer/2797468?hl=en)
+   * 
+   */
 
   override def getSearchResponse(searchText: String = "", latitude: String = "", longitude: String = "", radius: String = "", license: String = "", signRequest: Boolean = true): Response = {
     val searchRequest = new OAuthRequest(Verb.GET, endPointRootUri)
