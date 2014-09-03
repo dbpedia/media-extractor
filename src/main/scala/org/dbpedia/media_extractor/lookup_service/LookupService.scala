f2e2020ba484a84b82315849d432b6996290bc63
Leandro Doctors <ldoctors@gmail.com>
2014-09-03 18:26:40 +0200

    fixup! Delete GeoLookup support (only SemanticLookup will be supported)

--- src/main/scala/org/dbpedia/media_extractor/lookup_service/LookupService.scala
+++ src/main/scala/org/dbpedia/media_extractor/lookup_service/LookupService.scala
@@ -3,14 +3,19 @@ package org.dbpedia.media_extractor.lookup_service
 import org.dbpedia.media_extractor.media_lookup_service_provider.MediaLookupServiceProvider
 import org.scribe.builder.api.Api
 import org.scribe.model.Response
-
 import com.hp.hpl.jena.rdf.model.Model
-
 import javax.naming.directory.SearchResult
+import com.hp.hpl.jena.sparql.vocabulary.FOAF
+import com.hp.hpl.jena.vocabulary.RDF
+import com.hp.hpl.jena.vocabulary.RDFS
+import com.hp.hpl.jena.vocabulary.OWL
+import com.hp.hpl.jena.vocabulary.DCTerms
 
 abstract class LookupService[ProviderApi <: Api, SearchResultType <: SearchResult](
-  mediaLookupServiceProviderCallback: MediaLookupServiceProvider[ProviderApi, SearchResultType],
-  val radius: String = "5") {
+  // By default, search for Brussels
+  val targetResource: String = "Brussels",
+  val radius: String = "5",
+  mediaLookupServiceProviderCallback: MediaLookupServiceProvider[ProviderApi, SearchResultType]) {
 
   val lookupFooter = "Media Extractor (inspired by FlickrWrappr)"
 
@@ -22,13 +27,51 @@ abstract class LookupService[ProviderApi <: Api, SearchResultType <: SearchResul
 
   val signRequest = true
 
+  val dbpediaResourceRootUri = dbpediaRootUri + "resource/"
+  val dbpediaMediaResourceRootUri = dbpediaMediaRootUri + "resource/"
+
+  val resourceLeafUri = targetResource.trim.replaceAll(" ", "_").replaceAll("%2F", "/").replaceAll("%3A", ":")
+
+  val dbpediaResourceFullUri = dbpediaResourceRootUri + resourceLeafUri
+  val dbpediaMediaResourceFullUri = dbpediaMediaResourceRootUri + resourceLeafUri
+
   protected def namespaceUriMap = Map(
     "foaf" -> "http://xmlns.com/foaf/0.1/",
     "dcterms" -> "http://purl.org/dc/terms/",
     "rdfs" -> "http://www.w3.org/2000/01/rdf-schema#",
     "wgs84_pos" -> "http://www.w3.org/2003/01/geo/wgs84_pos#")
 
-  def addSearchResultsToRDFGraph[ResultType](searchResultsList: List[ResultType], rdfGraph: Model)
+  def performSemanticLookup(targetResource: String = "Brussels", radius: String = radius): Model
+
+  def addSearchResultsToRDFGraph(searchResultsList: List[SearchResult], rdfGraph: Model)
+
+  def addMetadataToRDFGraph(rdfGraph: Model) = {
+    addDocumentMetadataToRDFGraph(rdfGraph)
+  }
+
+  private def addDocumentMetadataToRDFGraph(rdfGraph: Model) = {
+
+    val lookupHeader = "Photos for Dbpedia resource " + targetResource
+
+    val lookupHeaderLiteral = rdfGraph.createLiteral(lookupHeader, "en")
+    val lookupFooterLiteral = rdfGraph.createLiteral(lookupFooter, "en")
+
+    val foafUri = namespaceUriMap("foaf")
+
+    val dbpediaResourceFullUriResource = rdfGraph.createResource(dbpediaResourceFullUri)
+    val dbpediaMediaResourceFullUriResource = rdfGraph.createResource(dbpediaMediaResourceFullUri)
+    val flickrTermsUriResource = rdfGraph.createResource(mediaLookupServiceProviderCallback.termsOfUseUri)
+    val dbpediaMediaRootUriResource = rdfGraph.createResource(dbpediaMediaRootUri)
+
+    dbpediaMediaRootUriResource.addProperty(RDFS.label, lookupFooterLiteral)
+    dbpediaMediaResourceFullUriResource.addProperty(RDFS.label, lookupHeaderLiteral)
+    dbpediaMediaResourceFullUriResource.addProperty(RDF.`type`, foafUri + "Document")
+    dbpediaMediaResourceFullUriResource.addProperty(FOAF.primaryTopic, dbpediaResourceFullUriResource)
+    dbpediaMediaResourceFullUriResource.addProperty(DCTerms.license, flickrTermsUriResource)
+    dbpediaMediaResourceFullUriResource.addProperty(FOAF.maker, dbpediaMediaRootUriResource)
+
+    dbpediaMediaResourceFullUriResource.addProperty(OWL.sameAs, dbpediaResourceFullUriResource)
+  }
 
   def validateSearchResponse(searchResponse: Response): Boolean = {
     searchResponse.getMessage().equals("OK")
