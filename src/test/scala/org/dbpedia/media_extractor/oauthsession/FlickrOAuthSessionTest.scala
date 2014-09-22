fc390d4eeeabe579d6059f3529f84fa36244993e
Leandro Doctors <ldoctors@gmail.com>
2014-09-22 12:12:43 +0200

    Add class "FlickrOAuthSessionTest"

---
+++ src/test/scala/org/dbpedia/media_extractor/oauthsession/FlickrOAuthSessionTest.scala
@@ -0,0 +1,29 @@
+package org.dbpedia.media_extractor.oauthsession
+
+import org.scalatest.FunSpec
+
+class FlickrOAuthSessionTest extends FunSpec {
+
+  describe("A FlickrOAuthSession instance") {
+
+    it("should be able to connect to Flickr using saved credentials (only <ApiKey,ApiKeySecret>)") {
+      val manuallyGeneratedFlickrOAuthSession = FlickrOAuthSession(
+        savedCredentialsFile = "/flickr.setup.properties",
+        savedAccessTokenFile = "")
+      assert(manuallyGeneratedFlickrOAuthSession.isInstanceOf[FlickrOAuthSession])
+    }
+
+    it("should be able to connect to Flickr using saved credentials (both <ApiKey,ApiKeySecret> and <accessToken,accessTokenSecret>)") {
+      val automaticallyGeneratedFlickrOAuthSession = FlickrOAuthSession(
+        savedCredentialsFile = "/flickr.setup.properties",
+        savedAccessTokenFile = "/flickr.accessToken.properties")
+      assert(automaticallyGeneratedFlickrOAuthSession.isInstanceOf[FlickrOAuthSession])
+
+      val generatedToken = automaticallyGeneratedFlickrOAuthSession.accessToken
+      val savedToken = automaticallyGeneratedFlickrOAuthSession.getSavedAccessToken("/flickr.accessToken.properties")
+      assert(generatedToken === savedToken)
+    }
+
+  }
+
+}
