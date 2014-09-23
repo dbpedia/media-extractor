547fdc3751fecfeba434fbeac5abed524e677408
Leandro Doctors <ldoctors@gmail.com>
2014-09-23 20:25:22 +0200

    Implement YouTubeOAuthSessionTest

--- src/test/scala/org/dbpedia/media_extractor/oauthsession/YouTubeOAuthSessionTest.scala
+++ src/test/scala/org/dbpedia/media_extractor/oauthsession/YouTubeOAuthSessionTest.scala
@@ -1,7 +1,29 @@
-package org.dbpedia.media_extractor.media_provider
+package org.dbpedia.media_extractor.oauthsession
 
 import org.scalatest.FunSpec
 
 class YouTubeOAuthSessionTest extends FunSpec {
+
+ describe("A YouTubeOAuthSession instance") {
+
+    it("should be able to connect to YouTube using saved credentials (only <ApiKey,ApiKeySecret>)") {
+      val manuallyGeneratedYouTubeOAuthSession = YouTubeOAuthSession(
+        savedCredentialsFile = "/youtube.setup.properties",
+        savedAccessTokenFile = "")
+      assert(manuallyGeneratedYouTubeOAuthSession.isInstanceOf[YouTubeOAuthSession])
+    }
+
+    it("should be able to connect to YouTube using saved credentials (both <ApiKey,ApiKeySecret> and <accessToken,accessTokenSecret>)") {
+      val automaticallyGeneratedYouTubeOAuthSession = YouTubeOAuthSession(
+        savedCredentialsFile = "/youtube.setup.properties",
+        savedAccessTokenFile = "/youtube.accessToken.properties")
+      assert(automaticallyGeneratedYouTubeOAuthSession.isInstanceOf[YouTubeOAuthSession])
+
+      val generatedToken = automaticallyGeneratedYouTubeOAuthSession.accessToken
+      val savedToken = automaticallyGeneratedYouTubeOAuthSession.getSavedAccessToken("/youtube.accessToken.properties")
+      assert(generatedToken === savedToken)
+    }
+
+  }
 
 }
