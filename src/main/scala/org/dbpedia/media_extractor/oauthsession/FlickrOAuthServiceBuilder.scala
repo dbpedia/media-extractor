d0d9ed16b74d6a0c68651dc09e5c0c76e4d7e3c3
Leandro Doctors <ldoctors@gmail.com>
2014-09-24 20:32:17 +0200

    BugFix: NullPointer: use "early definition" in *ServiceBuilder

--- src/main/scala/org/dbpedia/media_extractor/oauthsession/FlickrOAuthServiceBuilder.scala
+++ src/main/scala/org/dbpedia/media_extractor/oauthsession/FlickrOAuthServiceBuilder.scala
@@ -7,14 +7,15 @@ import org.scribe.oauth.OAuthService
 class FlickrOAuthServiceBuilder(
   myApiKey: String,
   myApiKeySecret: String)
-  extends OAuthServiceBuilder(myApiKey, myApiKeySecret) {
+  extends {
+
+    override val oAuthService: OAuthService = new ServiceBuilder()
+      .provider(classOf[FlickrApi])
+      .apiKey(myApiKey)
+      .apiSecret(myApiKeySecret)
+      .build()
+
+  } with OAuthServiceBuilder(myApiKey, myApiKeySecret) {
 
   override val providerName = "Flickr"
-
-  override val oAuthService: OAuthService = new ServiceBuilder()
-    .provider(classOf[FlickrApi])
-    .apiKey(myApiKey)
-    .apiSecret(myApiKeySecret)
-    .build()
-    
 }
--- src/main/scala/org/dbpedia/media_extractor/oauthsession/YouTubeOAuthServiceBuilder.scala
+++ src/main/scala/org/dbpedia/media_extractor/oauthsession/YouTubeOAuthServiceBuilder.scala
@@ -7,16 +7,17 @@ import org.scribe.oauth.OAuthService
 class YouTubeOAuthServiceBuilder(
   myApiKey: String,
   myApiKeySecret: String)
-  extends OAuthServiceBuilder(myApiKey, myApiKeySecret) {
+  extends {
+
+    override val oAuthService: OAuthService = new ServiceBuilder()
+      .provider(classOf[Google2Api])
+      .apiKey(myApiKey)
+      .apiSecret(myApiKeySecret)
+      .scope("https://www.googleapis.com/auth/youtube.readonly")
+      .callback("https://www.example.com/oauth2callback")
+      .build()
+
+  } with OAuthServiceBuilder(myApiKey, myApiKeySecret) {
 
   override val providerName = "YouTube"
-
-  override val oAuthService: OAuthService = new ServiceBuilder()
-    .provider(classOf[Google2Api])
-    .apiKey(myApiKey)
-    .apiSecret(myApiKeySecret)
-    .scope("https://www.googleapis.com/auth/youtube.readonly")
-    .callback("https://www.example.com/oauth2callback")
-    .build()
-    
 }
