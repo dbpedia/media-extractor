4108005b906a240887abd7fa5c8edd145efcd2c8
Leandro Doctors <ldoctors@gmail.com>
2017-03-04 15:37:48 -0300

    Delete all (Scala) code
    Good bye, Scala!

--- project/sbteclipse.sbt
+++
@@ -1,5 +0,0 @@
-addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "4.0.0")
-
-addSbtPlugin("com.mojolly.scalate" % "xsbt-scalate-generator" % "0.5.0")
-
-addSbtPlugin("org.scalatra.sbt" % "scalatra-sbt" % "0.3.5")
--- src/main/scala/ScalatraBootstrap.scala
+++
@@ -1,9 +0,0 @@
-import org.dbpedia.media_extractor.webapp._
-import org.scalatra._
-import javax.servlet.ServletContext
-
-class ScalatraBootstrap extends LifeCycle {
-  override def init(context: ServletContext) {
-    context.mount(new MyScalatraServlet, "/*")
-  }
-}
