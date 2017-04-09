78e6ffff9f0c37b101f0cee1e90c112de2b66ec7
Leandro Doctors <ldoctors@gmail.com>
2017-04-09 19:48:41 -0300

    Write input test (stub)

--- test/dbpedia_media_extractor/io_test.clj
+++ test/dbpedia_media_extractor/io_test.clj
@@ -2,6 +2,6 @@
   (:require [clojure.test :refer :all]
             [dbpedia-media-extractor.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest input-reading-test
+  (testing "Parsing a file."
+    (is (= generated-data mocked-data))))
