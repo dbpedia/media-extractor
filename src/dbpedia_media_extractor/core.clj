1cb13cfc3d252219bd596207d207f181cc67bf50
Leandro Doctors <ldoctors@gmail.com>
2017-04-10 14:13:04 -0300

    Add "parse" fn

--- src/dbpedia_media_extractor/core.clj
+++ src/dbpedia_media_extractor/core.clj
@@ -1,6 +1,12 @@
 (ns dbpedia-media-extractor.core
   (:gen-class))
 
+(defn parse
+  "Convert a CSV into rows of columns"
+  [string]
+  (map #(clojure.string/split % #",")
+       (clojure.string/split string #"\n")))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
