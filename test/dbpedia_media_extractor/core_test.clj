a115e5f789fe77e3541e0e10bc2c70349e965b14
Leandro Doctors <ldoctors@gmail.com>
2017-04-10 14:05:50 -0300

    Testing parser: hand-made version... that fails :-)

--- test/dbpedia_media_extractor/core_test.clj
+++ test/dbpedia_media_extractor/core_test.clj
@@ -4,4 +4,9 @@
 
 (deftest input-reading-test
   (testing "Parsing a file."
-    (is (= generated-data mocked-data))))
+    (require '[clojure.java.io :as io])
+    (def raw-data "name,age\nBart,10\nLisa,8")
+    (def tmp-filename "tmp/simpson_kids.csv")
+    (spit tmp-filename raw-data)
+    (def correctly-parsed-data (["name" "age"] ["Bart" "10"] ["Lisa" "8"]))
+    (is (= (parse(tmp-filename)) (correctly-parsed-data)))))
