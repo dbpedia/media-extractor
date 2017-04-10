68bb0a22bca82c5a8e5a22492328a22a4417136b
Leandro Doctors <ldoctors@gmail.com>
2017-04-10 16:56:38 -0300

    input-parsing-test now works :-)
    Thanks a lot, Dan! (@danmidwood)

--- test/dbpedia_media_extractor/core_test.clj
+++ test/dbpedia_media_extractor/core_test.clj
@@ -1,14 +1,13 @@
 (ns dbpedia-media-extractor.core-test
   (:require [clojure.test :refer :all]
-            [dbpedia-media-extractor.core :refer :all]))
+            [dbpedia-media-extractor.core :refer :all]
+            [clojure.java.io :as io]))
 
-(deftest input-reading-test
+(deftest input-parsing-test
   (testing "Parsing a file."
-    (require [clojure.java.io :as io])
-    (def raw-data "name,age\nBart,10\nLisa,8")
-    (def tmp-filename "tmp/simpson_kids.csv")
-    (spit tmp-filename raw-data)
-    (def read-data (slurp tmp-filename))
-    (def correctly-parsed-data (list ["name" "age"] ["Bart" "10"] ["Lisa" "8"]))
-    (is (= (parse read-data) (correctly-parsed-data)))
-    (io/delete-file tmp-filename)))
+    (let [raw-data "name,age\nBart,10\nLisa,8"
+          tmp-filename "./simpson_kids.csv"
+          correctly-parsed-data '(["name" "age"] ["Bart" "10"] ["Lisa" "8"])]
+      (spit tmp-filename raw-data)
+      (is (= (parse (slurp tmp-filename)) correctly-parsed-data) "Testing the parser")
+      (io/delete-file tmp-filename))))
