a4b93858e361363db9a0d47f93e580600251588b
Leandro Doctors <ldoctors@gmail.com>
2017-05-11 17:03:25 -0300

    Add mapify fn

--- src/dbpedia_media_extractor/core.clj
+++ src/dbpedia_media_extractor/core.clj
@@ -7,6 +7,17 @@
   (map #(clojure.string/split % #",")
        (clojure.string/split string #"\n")))
 
+(defn mapify
+  "Convert a seq of rows (vectors) of string pairs into a seq of maps like {:name \"Edward Cullen\" :glitter-index \"10\"}"
+  [rows]
+  (let [my-keywords (map keyword (first rows))]
+       (map (fn [unmapped-row]
+              (reduce (fn [row-map [my-key my-value]]
+                        (assoc row-map my-key my-value))
+                      {}
+                      (map vector my-keywords unmapped-row)))
+            (rest rows))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
