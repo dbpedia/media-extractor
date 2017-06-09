c164d1651170562af97cc6ae2f7fc2722089686a
Leandro Doctors <ldoctors@gmail.com>
2017-06-09 15:28:18 -0300

    Debugging: Adding 'print' statements

--- test/dbpedia_media_extractor/core_test.clj
+++ test/dbpedia_media_extractor/core_test.clj
@@ -29,28 +29,49 @@
 
     (def mapped-login-credentials (first (mapify (parse (slurp credentials-csv-file)))))
 
+    (println "mapped-login-credentials (inline): " (first(mapify(parse(slurp credentials-csv-file)))))
+
+    (println "mapped-login-credentials (pre-calculated): " mapped-login-credentials)
+
     (def my-api-key (:api_key mapped-login-credentials))
+
+    (println "my-api-key (inline): " (:api_key mapped-login-credentials))
+    (println "my-api-key (pre-calculated): " my-api-key)
+
     (def my-api-secret (:secret mapped-login-credentials))
+
+    (println "my-api-secret (inline): " (:secret mapped-login-credentials))
+    (println "my-api-secret (pre-calculated): " my-api-secret)
+
 
     (def conf {:type       :scribe
                :provider   org.scribe.builder.api.FlickrApi
                :api-key    my-api-key
                :api-secret my-api-secret})
 
+    (println "conf: " conf)
+
     (def service (oauth/build conf))
 
-    (let [rec               (oauth/new-record service)
-          _                 (println "Auth url:" (:url rec))
+    (println "service: " service)
+
+    ;; FIXME: the following line fails.
+    ;; It should go inside the 'let', but I took it out for debugging purposes.
+    (println "rec (inline): " (oauth/new-record service))
+
+    (comment(let [rec               (oauth/new-record service)
+          _                 (println "rec: " rec)
+          _                 (println "Auth url: " (:url rec))
           _                 (print "Enter token: ")
           _                 (flush)
           token             (clojure.string/trim (read-line))
-          _                 (println "your token:" token)
+          _                 (println "your token: " token)
           rec               (oauth/activate service rec token)
           resp              ((oauth/requestor service rec)
                              {:url "https://api.flickr.com/services/rest/"})
           flickr-test-login (-> resp :body clojure.data.xml/parse-str
                                 :content first :content first)
-          _                 (println "full response:" resp)
-          _                 (println "response status:" (:status resp))
-          _                 (println "response headers:" (pr-str (:headers resp)))
-          _                 (println "flickr-test-login:" flickr-test-login)])))
+          _                 (println "full response: " resp)
+          _                 (println "response status: " (:status resp))
+          _                 (println "response headers: " (pr-str (:headers resp)))
+          _                 (println "flickr-test-login:" flickr-test-login)]))))
