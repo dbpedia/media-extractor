6259727e3a916fd34970e82f478dae7309c4aa6f
Leandro Doctors <ldoctors@gmail.com>
2017-06-13 11:33:58 -0300

    Use let's (instead of def's) in all tests

--- test/dbpedia_media_extractor/core_test.clj
+++ test/dbpedia_media_extractor/core_test.clj
@@ -9,18 +9,20 @@
 
 (deftest input-parsing-test
   (testing "Parsing a file"
-    (def raw-data "name,age\nBart,10\nLisa,8")
-    (def tmp-filename "simpson_kids.csv")
-    (def correctly-parsed-data '(["name" "age"] ["Bart" "10"] ["Lisa" "8"]))
-    (spit tmp-filename raw-data)
-    (is (= (parse (slurp tmp-filename)) correctly-parsed-data) "Testing the parser")
-    (io/delete-file tmp-filename)))
+    (let
+      [raw-data "name,age\nBart,10\nLisa,8"
+        tmp-filename "simpson_kids.csv"
+        correctly-parsed-data '(["name" "age"] ["Bart" "10"] ["Lisa" "8"])]
+      (spit tmp-filename raw-data)
+      (is (= (parse (slurp tmp-filename)) correctly-parsed-data) "Testing the parser")
+      (io/delete-file tmp-filename))))
 
 (deftest input-mapifying-test
   (testing "Mapifying a CSV file (with header)"
-    (def raw-data '(["name" "age"] ["Bart" "10"] ["Lisa" "8"]))
-    (def correctly-mapified-data '({:name "Bart", :age "10"} {:name "Lisa", :age "8"}))
-    (is (= (mapify raw-data) correctly-mapified-data))))
+    (let
+      [raw-data '(["name" "age"] ["Bart" "10"] ["Lisa" "8"])
+        correctly-mapified-data '({:name "Bart", :age "10"} {:name "Lisa", :age "8"})]
+      (is (= (mapify raw-data) correctly-mapified-data)))))
 
 (deftest flickr-oauth-login
   (testing "Logging into Flickr"
