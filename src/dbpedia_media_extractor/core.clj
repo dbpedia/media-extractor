8a6e6f3ca4b15c0fe7036aa7cb04818db2223a33
Leandro Doctors <ldoctors@gmail.com>
2017-09-01 14:01:00 -0300

    Transform 'generate-access-token' <-'flickr-test-login'

--- src/dbpedia_media_extractor/core.clj
+++ src/dbpedia_media_extractor/core.clj
@@ -18,6 +18,34 @@
                    (map vector my-keywords unmapped-row)))
          (rest rows))))
 
+(defn generate-access-token
+  "Generates an access token vector, based on credentials stored in a CSV file. Needs interaction to get the authorization."
+  [stored-credentials-csv-file] ;; This one should be "resources/flickr_keys.csv"
+  (let [mapped-login-credentials  (first (mapify (parse (slurp stored-credentials-csv-file))))
+        my-api-key                (:api_key mapped-login-credentials)
+        my-api-secret             (:api_secret mapped-login-credentials)
+        #_                         (println "my-api-key: " my-api-key)
+        #_                         (println "my-api-secret: " my-api-secret)
+        conf                      {:type :scribe, :provider org.scribe.builder.api.FlickrApi, :api-key my-api-key, :api-secret my-api-secret}
+        #_                         (println "conf: " conf)
+        service                   (oauth/build conf)
+        #_                         (println "service: " service)
+        rec                       (oauth/new-record service)
+        #_                         (println "rec: " rec)
+        _                         (println "Please, follow the link below to authorize this App on Flickr.
+                                            Once that is done, please enter the token obtained from Flickr."
+                                           \n "Authorization URL: "(:url rec)
+                                           \n "Token ('NNN-NNN-NNN'):")
+        _                         (flush)
+        token                     (clojure.string/trim (read-line))
+        _                         (println "Thank you!")
+        #_                         (println "This is the token you obtained: " token)
+        rec                       (oauth/activate service rec token)
+        #_                         (println "Authorized rec: " rec)
+        access-token              (:access-token rec)
+        #_                         (println "This is the Access Token (to be stored): " access-token)]
+    access-token))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
