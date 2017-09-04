6a5f2694c886baa369338809c5981949149b84ce
Leandro Doctors <ldoctors@gmail.com>
2017-09-04 18:55:00 -0300

    Fix tempfile location

--- test/dbpedia_media_extractor/core_test.clj
+++ test/dbpedia_media_extractor/core_test.clj
@@ -6,7 +6,7 @@
 (deftest input-parsing-test
   (testing "Parsing a file"
     (let [raw-data "name,age\nBart,10\nLisa,8"
-          tmp-filename "simpson_kids.csv"
+          tmp-filename "resources/simpson_kids.csv"
           correctly-parsed-data '(["name" "age"] ["Bart" "10"] ["Lisa" "8"])]
       (spit tmp-filename raw-data)
       (is (= (parse (slurp tmp-filename)) correctly-parsed-data) "Testing the parser")
